4ffe8d4de602944a3c07ff187207b4315d7dcecb
Danny Shen <shenfive@gmail.com>
2018-05-17 14:22:21 +0800

    initional
    初始化

---
+++ app/src/main/java/android/idv/sjw/helloemailauth/MainActivity.java
@@ -0,0 +1,13 @@
+package android.idv.sjw.helloemailauth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
